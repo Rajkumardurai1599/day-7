13fc4ed628d730e61ad54c80a0dcc460ca8238c9
Rajkumardurai1599 <drajkumar1599@gmail.com>
2024-04-08 15:32:50 +0530

    updated task

--- script.js
+++ script.js
@@ -1,47 +1,45 @@
 const request = new XMLHttpRequest();
-request.open("GET","https://restcountries.com/v3.1/all",true)
+request.open("GET", "https://restcountries.com/v3.1/all", true);
 request.send();
-request.onload=function(){
-    var data = request.response;
-    var result = JSON.parse(data)
-    /*console.log(result)*/
+request.onload = function () {
+  var data = request.response;
+  var result = JSON.parse(data);
+  /*console.log(result)*/
 
-    //(a)Get all the countries from Asia continent /region using Filter function
+  //(a)Get all the countries from Asia continent /region using Filter function
 
+  var res = result.filter((ele) => ele.region === "Asia");
 
-    var res = result.filter((ele)=>ele.region==="Asia")
-    
+  var final = res.map((ele) => console.log(ele.name.common));
 
-    var final = res.map((ele)=>console.log(ele.name.common))   
+  //(b)get all the countries with a population of less than 2 lakhs using Filter function
 
-    /*(b)get all the countries with a population of less than 2 lakhs using Filter function
+  var pop = result.filter((ele) => ele.population < 200000);
 
-var pop  = result.filter((ele)=>ele.population<200000)
+  pop.forEach((value) => console.log(value.name.common));
 
-pop.forEach((value)=>console.log(value.name.common))*/
+  //(c)Print the following details name, capital, flag, using forEach function
 
-/*(c)Print the following details name, capital, flag, using forEach function
-var countryData=JSON.parse(this.response);
-countryData.forEach((element)=>{
-    console.log(element.name,element.capital,element.flag,);
-})*/
+  result.forEach((element) => {
+    console.log(
+      "Name",
+      element.name,
+      "Capital",
+      element.capital,
+      "flag",
+      element.flag
+    );
+  });
 
-/*(d)Print the total population of countries using reduce function
+  //(d)Print the total population of countries using reduce function
 
-var countryData=JSON.parse(this.response);
-const population=countryData.reduce((acc,element)=>{
-    return acc+element.population;
-},0)
-console.log(population);*/
+  var population = result.reduce((acc, element) => {
+    return acc + element.population;
+  }, 0);
+  console.log(population);
 
+  // (e)Print the country that uses US dollars as currency.
 
-
-/* (e)Print the country that uses US dollars as currency.
-
-var curr  = result.filter((ele)=>ele.currencies && ele.currencies.USD)
-curr.forEach((value)=>console.log(value.name.common))*/
-
-
-}
-
-
+  var curr = result.filter((ele) => ele.currencies && ele.currencies.USD);
+  curr.forEach((value) => console.log(value.name.common));
+};
